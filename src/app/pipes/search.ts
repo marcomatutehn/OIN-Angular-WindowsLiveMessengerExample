12f5ec40a17b6cb9245ee3aa6bbfbfd8a666b7d6
MarcoPC <marcomatute@unah.hn>
2019-07-07 20:51:52 -0600

    Pipe lesson completed

--- src/app/pipes/search.ts
+++ src/app/pipes/search.ts
@@ -5,13 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class SearchPipe implements PipeTransform {
+    //Value sera la lista de objetos ( Lista de amigos ) y Args sera el query string ( Ejemplo Ricardo)
     public transform(value: any, args: string): any {
+        //Regresa un return por que viene vacio el arreglo de value
         if(!value) {
             return;
         }
+        //Regresa todo el arreglo
         if (!args) {
             return value;
         }
+        //compara el elemento 
         args = args.toLowerCase();
         return value.filter ( (item) => {
             return JSON.stringify(item).toLowerCase().includes(args);
